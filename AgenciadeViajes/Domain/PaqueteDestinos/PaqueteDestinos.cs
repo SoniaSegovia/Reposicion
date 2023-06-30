45bbd05839b1498dd963bea3986b382425bb8509
SoniaSegovia <95039528+SoniaSegovia@users.noreply.github.com>
2023-06-29 21:43:16 -0600

    agregar cambios

--- AgenciadeViajes/Domain/PaqueteDestinos/PaqueteDestinos.cs
+++ AgenciadeViajes/Domain/PaqueteDestinos/PaqueteDestinos.cs
@@ -4,6 +4,9 @@ namespace Domain.PaqueteDestinos
 {
     public sealed class PaqueteDestinos : AggregateRoot
     {
+        private global::System.Boolean activo1 = default(;
+
+
         public PaqueteDestinos(PaqueteDestinosId id,int destinoId,int paqueteId,Destinos destinos,Paquetes paquetes,bool activo){
             this.Id = id;
             this.destinoId = destinoId;
@@ -13,11 +16,13 @@ namespace Domain.PaqueteDestinos
             this.Activo = activo;
         }
 
-        public PaqueteDestinosId Id {get;private set;};
-        public int paqueteId {get;private set;};
-        public Paquetes paquete {get;private set;};
-        public int destinoId {get;private set;};
-        public Destinos destino {get;private set;};
+        public PaqueteDestinosId Id {get;private set;}
+        public int paqueteId {get;private set;}
+        public Paquetes paquete {get;private set;}
+        public int destinoId {get;private set;}
+        public Destinos destino {get;private set;}
+
+        public bool activo { get => activo1; private set => activo1 = value; }
 
         public bool activo {get; private set;}; = default(true);
     }
