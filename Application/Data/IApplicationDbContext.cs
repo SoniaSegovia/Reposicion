bc68128338045fbd6c1b3ec1a109eadf2d7a25b4
SoniaSegovia <95039528+SoniaSegovia@users.noreply.github.com>
2023-06-29 21:06:30 -0600

    agregar cambios

---
+++ Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Destinos;
+using Domain.Paquetes;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Data
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Domain.Destinos.Destinos> Destinos { get; set; }
+        DbSet<Paquetes> Paquetes { get; set; }
+        DbSet<PaqueteDestinos> PaqueteDestinos { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken=default);
+    }
+}
